36e4d05ba06a1cd991760813750343a5a3bde0fa
petros <pit.trak@gmail.com>
2020-08-30 18:51:36 +0300

    Connected to mongoDB with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+
+mongoose
+    .connect(db)
+    .then(() => console.log("mongoDB connected!"))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello!'));
 
