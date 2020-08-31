025692d7a082b2eb35edbe46dc1e4adddba9f403
petros <pit.trak@gmail.com>
2020-09-01 00:11:02 +0300

    Added profile field validations

--- server.js
+++ server.js
@@ -9,31 +9,30 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-// Body Parser
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connect to mongoDB
-
+// Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log("mongoDB connected!"))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
-// Passport config
+// Passport Config
 require('./config/passport')(passport);
 
-// User routes
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running at ${port}!`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
