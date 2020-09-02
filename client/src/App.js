a9152b2a49dd232c09379d2897d538661f21f9b3
petros <pit.trak@gmail.com>
2020-09-02 23:40:39 +0300

    Added login form functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import "./App.css";
 import { Provider } from 'react-redux';
 import store from './store';
@@ -9,6 +12,16 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
+// Check for token
+if(localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken)
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken)
+  // Set user in isAuthenticated
+  store.dispatch(setCurrentUser(decoded))
+}
 
 function App() {
   return (
