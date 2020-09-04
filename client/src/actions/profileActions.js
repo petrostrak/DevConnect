21303cefa18e7a231aeade964142055ee74b1023
petros <pit.trak@gmail.com>
2020-09-04 17:09:20 +0300

    Created dashboard experience display and delete function

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -54,6 +54,24 @@ export const addEducation = (eduData, history) => dispatch => {
         )
 }
 
+// delete experience
+export const deleteExperience = id => dispatch => {
+    axios
+      .delete(`/api/profile/experience/${id}`)
+      .then(res =>
+        dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  };
+
 // delete account & profile
 export const deleteAccount = () => dispatch => {
     if(window.confirm('Are you sure? This cannot be undone!')) {
